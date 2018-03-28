647d87ebb04b80a3ef6b9b1fa5abc763ec3d6d8b
Rene Noel <rnoel@sapient.com>
2018-03-28 16:02:52 -0400

    show/hide scripting

--- components/global-nav/global-nav.js
+++ components/global-nav/global-nav.js
@@ -3,6 +3,17 @@ var menuItems = document.querySelectorAll('li.has-submenu');
 Array.prototype.forEach.call(menuItems, function(el, i){
 	el.querySelector('a').addEventListener("focus",  function(event){
 		if (this.parentNode.className == "has-submenu") {
+
+			var openMenuItems = document.querySelectorAll('li.has-submenu.open');
+
+			console.log(openMenuItems);
+
+			[].forEach.call(openMenuItems, function(item) {
+				console.log('looping', item);
+				item.className = "has-submenu";
+				item.querySelector('a').setAttribute('aria-expanded', "false");
+			});
+
 			this.parentNode.className = "has-submenu open";
 			this.setAttribute('aria-expanded', "true");
 		} else {
@@ -12,4 +23,16 @@ Array.prototype.forEach.call(menuItems, function(el, i){
 		event.preventDefault();
 		return false;
 	});
+
+	// el.querySelector('a').addEventListener("blur",  function(event){
+	// 	if (this.parentNode.className == "has-submenu open") {
+	// 		this.parentNode.className = "has-submenu";
+	// 		this.setAttribute('aria-expanded', "false");
+	// 	} else {
+	// 		this.parentNode.className = "has-submenu open";
+	// 		this.setAttribute('aria-expanded', "true");
+	// 	}
+	// 	event.preventDefault();
+	// 	return false;
+	// });
 });
