ec79828af188c4a88e0b1354b42f715e1358f509
krinakam@publicisgroupe.net <knakamura2@sapient.com>
2018-03-28 15:49:37 -0400

    tabbing functionality

--- components/global-nav/global-nav.js
+++ components/global-nav/global-nav.js
@@ -1,8 +1,16 @@
 /* Global Nav Functions */
 var menuItems = document.querySelectorAll('li.has-submenu');
+
 Array.prototype.forEach.call(menuItems, function(el, i){
-	el.querySelector('a').addEventListener("focus",  function(event){
+	el.querySelector('a').addEventListener("click",  function(event){
+		event.preventDefault();
+		
 		if (this.parentNode.className == "has-submenu") {
+			// var openMenuItem = document.querySelector('li.has-submenu.open');
+
+			// openMenuItem.className = "has-submenu";
+			// openMenuItem.setAttribute('aria-expanded', "false");
+
 			this.parentNode.className = "has-submenu open";
 			this.setAttribute('aria-expanded', "true");
 		} else {
