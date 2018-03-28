8b073fe73dbfd6b114108f94c8f772575fad8f3b
Rene Noel <rnoel@sapient.com>
2018-03-28 15:20:42 -0400

    Nav adjustments: WIP

--- components/global-nav/global-nav.js
+++ components/global-nav/global-nav.js
@@ -1,1 +1,15 @@
 /* Global Nav Functions */
+var menuItems = document.querySelectorAll('li.has-submenu');
+Array.prototype.forEach.call(menuItems, function(el, i){
+	el.querySelector('a').addEventListener("focus",  function(event){
+		if (this.parentNode.className == "has-submenu") {
+			this.parentNode.className = "has-submenu open";
+			this.setAttribute('aria-expanded', "true");
+		} else {
+			this.parentNode.className = "has-submenu";
+			this.setAttribute('aria-expanded', "false");
+		}
+		event.preventDefault();
+		return false;
+	});
+});
