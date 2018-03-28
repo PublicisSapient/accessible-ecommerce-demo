cc23475518d9422cc22ca3c2c00405c4660de191
David Jackson <djackson@sapient.com>
2018-03-28 15:58:17 -0400

    add showing number of products aria live region

--- components/product-grid/product-grid.js
+++ components/product-grid/product-grid.js
@@ -1,1 +1,8 @@
 /* Product Grid Functions */
+var clearFiltersButton = document.querySelector('#showingNumberProducts__clear');
+var numberOfProductsInGridAriaLiveRegion = document.querySelector('#showingNumberProducts__description');
+
+// Add click event to clear filters button
+clearFiltersButton.addEventListener('click', function(event) {
+  numberOfProductsInGridAriaLiveRegion.innerHTML = 'Showing ' + 80 + ' products.';
+});
