98004c0a269f5e232d6d27abe7843e9770d6d768
Zoltan Hawryluk <zoltan.dulac@gmail.com>
2018-03-28 15:31:08 -0400

    Basic interactivity of focus/blur

--- components/mini-cart/mini-cart.js
+++ components/mini-cart/mini-cart.js
@@ -1,1 +1,65 @@
 /* Mini Cart Functions */
+
+const MiniCart = new function () {
+    const rootClass = 'mini-cart'
+    const el = document.getElementsByClassName(rootClass)[0];
+    const buttonEl = el.getElementsByClassName(`${rootClass}__button`)[0];
+    const dropdownEl = el.getElementsByClassName(`${rootClass}__dropdown`)[0];
+    this.init = function () {
+        this.onFocus = this.onFocus.bind(this);
+        this.onBlur = this.onBlur.bind(this);
+        this.onButtonBlur = this.onButtonBlur.bind(this);
+        this.closeIfMenuBlurred = this.closeIfMenuBlurred.bind(this);
+
+        console.log(el, buttonEl, dropdownEl);
+        buttonEl.addEventListener('mouseover', this.onFocus);
+        buttonEl.addEventListener('mouseout', this.onBlur);
+
+        buttonEl.addEventListener('focus', this.onFocus);
+        buttonEl.addEventListener('blur', this.onButtonBlur);
+        
+        dropdownEl.addEventListener('blur', this.onButtonBlur);
+    }
+
+    this.onFocus = function () {
+        console.log('focus');
+        dropdownEl.classList.add('expanded');
+    }
+
+    this.onBlur = function () {
+        console.log('onBlur');
+        dropdownEl.classList.remove('expanded');
+    }
+
+    this.onButtonBlur = function (e) {
+        console.log('onButtonBlur');
+        requestAnimationFrame(
+			this.closeIfMenuBlurred.bind(this, e.target, e.currentTarget, e.relatedTarget)
+		);
+    }
+
+    this.closeIfMenuBlurred = function (target, currentTarget, relatedTarget) {
+        console.log('closeIfMenuBlurred', relatedTarget, document.activeElement);
+        const focusedElement = relatedTarget || document.activeElement;
+		const isFocusLost = (
+			focusedElement.parentNode === document.body ||
+			focusedElement === document.body ||
+			focusedElement === null
+		);
+
+		/*
+		 * If a user clicks anywhere within the menu that isn't a button, it
+		 * shouldn't close the menu.  This happens also should happen when focus is
+		 * lost (which is what the `isFocusLost` variable keeps track of).
+		 *
+		 * If we blurred out of the `.menu_panel`, then we must close the
+		 * menu.
+		 */
+        console.log(isFocusLost)
+		if (/* !isFocusLost &&*/ !dropdownEl.contains(focusedElement)) {
+			this.onBlur();
+		}
+	}
+}
+
+MiniCart.init();
