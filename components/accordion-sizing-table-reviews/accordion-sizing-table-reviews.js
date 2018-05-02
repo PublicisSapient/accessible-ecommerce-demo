db1064fffca750f5e4d2fded824ff4e7fc056b06
David Jackson <djackson@sapient.com>
2018-05-02 15:54:36 -0400

    add click events

--- components/accordion-sizing-table-reviews/accordion-sizing-table-reviews.js
+++ components/accordion-sizing-table-reviews/accordion-sizing-table-reviews.js
@@ -1,13 +1,31 @@
 // initialize accordion events
-function onClickToggleAccordionPanel(accordionPanel) {
-  console.log('creating click event');
-  return function(event) {
-    var accordionPanelContent = accordionPanel.querySelector('.accordion-panel_content');
-    accordionPanelContent.classList.toggle('accordion--open');
+
+function setAriaLabelForButton(accordionPanel) {
+  var accordionPanelContent = accordionPanel.querySelector('.accordion-panel_content');
+  var accordionPanelButton = accordionPanel.querySelector('button');
+
+  if(accordionPanelContent.classList.contains('accordion--open')) {
+    accordionPanelButton.setAttribute('aria-label', accordionPanelButton.getAttribute('data-hide-aria-label'));
+  } else {
+    accordionPanelButton.setAttribute('aria-label', accordionPanelButton.getAttribute('data-open-aria-label'));
   }
 }
+
+function onClickToggleAccordionPanel(accordionPanel) {
+  return function(event) {
+    var accordionPanelContent = accordionPanel.querySelector('.accordion-panel_content');
+    var accordionPanelButton = accordionPanel.querySelector('button');
+
+    // toggle the open class
+    accordionPanelContent.classList.toggle('accordion--open');
+    
+    setAriaLabelForButton(accordionPanel);
+  }
+}
+
 var accordionPanels = document.querySelectorAll('.accordion-panel');
 accordionPanels.forEach(function(accordionPanel) {
   var button = accordionPanel.querySelector('button');
+  setAriaLabelForButton(accordionPanel);
   button.addEventListener('click', onClickToggleAccordionPanel(accordionPanel));
 });
