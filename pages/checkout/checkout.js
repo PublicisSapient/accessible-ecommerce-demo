4a6971ca96ce7a1365fd7f493c33916a3af4871c
Alison Hall <ahall7@sapient.com>
2018-07-18 12:38:19 -0400

    Get 'next' button working

--- pages/checkout/checkout.js
+++ pages/checkout/checkout.js
@@ -1,21 +1,5 @@
 var billingCheckbox = document.getElementById('same-as-shipping');
 
-function goToAccordionPanel(goToPanel) {
-	// event.preventDefault();
-	// console.log('HERE');
-	// // var accordionPanelContent = closePanel.querySelector('.accordion-panel_content');
-	// // var accordionPanelHeading = closePanel.querySelector('.accordion-panel_heading');
-
-	// var openAccordionPanels = document.querySelectorAll('.accordion--open');
-	// console.log(openAccordionPanels);
-	// openAccordionPanels.forEach(function(openPanel) {
-	// 	console.log(openPanel);
-	// 	openPanel.classList.remove('accordion--open');
-	// });
-
-	onClickToggleAccordionPanel(goToPanel)
-}
-
 billingCheckbox.addEventListener('change', function() {
 	var billingAddressFields = document.getElementById('billing-address');
 	billingAddressFields.classList.toggle('billing-address-hidden');
@@ -24,4 +8,4 @@ billingCheckbox.addEventListener('change', function() {
 var goToPaymentDetailsButton = document.getElementById('go-to-payment-details');
 var shippingAccordionPanel = document.getElementById('shipping-accordion-panel');
 var paymentDetailsAccordionPanel = document.getElementById('payment-details-accordion-panel');
-goToPaymentDetailsButton.addEventListener('click', goToAccordionPanel(paymentDetailsAccordionPanel));
+goToPaymentDetailsButton.addEventListener('click', onClickToggleAccordionPanel(paymentDetailsAccordionPanel));
