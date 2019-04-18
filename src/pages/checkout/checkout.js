dd80e68321293cb2887ee79c19d37009338d41bf
Darren Neville <dneville@sapient.com>
2019-04-18 15:12:24 -0400

    Adding payment form

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -1,71 +1,51 @@
-const checkoutPage = (function() {
-  let signInSubmit;
-  let guestSignInSubmit;
-  let shippingSubmit;
+const checkForm = (e) => {
+  e.preventDefault();
+  const form = e.currentTarget.parentNode;
+  const inputs = form.querySelectorAll('input, select');
 
-  const checkForm = (e) => {
-    e.preventDefault();
-    const form = e.currentTarget.parentNode;
-    const inputs = form.querySelectorAll('input, select');
-    const emailRegex = new RegExp(/^([A-Za-z0-9\-\/\:\;\(\)\$\&\"\=\,\?\*\#\%\^\+\_\.\|\[\]\{\}\<\>\\\')+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,16})$/);
-    let formValid = true;
+  const regex = {
+    email: /^([A-Za-z0-9\-\/\:\;\(\)\$\&\"\=\,\?\*\#\%\^\+\_\.\|\[\]\{\}\<\>\\\')+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,16})$/,
+    'postal-code': /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/,
+    'phone-number': /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
+    'credit-card-number': /^$[0-9]{14}/,
+    'expiry-date': /^[0-9]{2}[-\s\/\\]?[0-9]{2}$/,
+    ccv: /^[0-9]{3,4}$/,
+  };
 
-    inputs.forEach(({ type, classList, dataset, value }) => {
-      let valid = false;
-      const { required } = dataset;
+  let formValid = true;
 
-      if (type === 'select') {
-        console.log(value);
+  inputs.forEach(({ classList, dataset, value }) => {
+    let valid = false;
+    const { required, inputType } = dataset;
+
+    if (required) {
+      valid = regex[inputType] ? regex[inputType].test(value) : Boolean(value);
+
+      if (valid) {
+        classList.remove('invalid');
+      } else {
+        classList.add('invalid');
+        formValid = false;
       }
-
-      if (required) {
-        switch(type) {
-          case 'email':
-            valid = emailRegex.test(value);
-            break;
-          case 'select':
-          case 'password':
-          case 'text':
-            valid = Boolean(value);
-            break;
-          default:
-        }
-
-        if (valid) {
-          classList.remove('invalid');
-        } else {
-          classList.add('invalid');
-          formValid = false;
-        }
-      }
-    });
-
-    if (formValid) {
-      form.submit();
     }
-  };
+  });
 
-  const addListeners = () => {
-    signInSubmit.addEventListener('click', checkForm);
-    guestSignInSubmit.addEventListener('click', checkForm);
-    shippingSubmit.addEventListener('click', checkForm);
-  };
+  formValid && form.submit();
+};
 
-  const defineEls = () => {
-    signInSubmit = document.querySelector('.sign-in-form__submit');
-    guestSignInSubmit = document.querySelector('.guest-sign-in__submit');
-    shippingSubmit = document.querySelector('.shipping-form__submit');
-  };
+const addListeners = (submitButtons = []) => {
+  submitButtons.forEach(button => {
+    button && button.addEventListener('click', checkForm);
+  });
+};
 
-  const init = () => {
-    defineEls();
-    addListeners();
-  };
+const findButtons = (...selectors) => selectors.map(button => document.querySelector(button));
 
-  return {
-    init,
-  }
-})();
-
-window.onload = checkoutPage.init;
-
+window.onload = () => addListeners(
+  findButtons(
+    '.sign-in-form__submit',
+    '.guest-sign-in__submit',
+    '.shipping-form__submit',
+    '.payment-information__submit'
+  )
+);
