7f33c98f06e3da3f00f1f61ea8368be000e42226
Brian Elton <brian.elton@sapientrazorfish.com>
2019-05-02 11:28:35 -0400

    updating modal implementation to make use of existing functionality

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,6 @@
 import headerTemplate from './components/header/header.hbs';
 import footerTemplate from './components/footer/footer.hbs';
-import modalTemplate from './components/modal/modal.hbs';
+import constructionModalTemplate from './components/modal/construction-modal.hbs';
 import * as MiniCart from './components/mini-cart/mini-cart';
 import * as GlobalNav from './components/header/global-navigation';
 import * as Modal from './components/modal/modal';
@@ -8,12 +8,12 @@ import * as Modal from './components/modal/modal';
 // Global templating
 const headerDOM = document.querySelector('[data-template="global-header"]');
 const footerDOM = document.querySelector('[data-template="footer"]');
-const modalDOM = document.querySelector('[data-template="modal"]');
+const constructionModalDOM = document.querySelector('[data-template="construction-modal"]');
 if (headerDOM) headerDOM.outerHTML = headerTemplate();
 if (footerDOM) footerDOM.outerHTML = footerTemplate();
-if (modalDOM) modalDOM.outerHTML = modalTemplate();
+if (constructionModalDOM) constructionModalDOM.outerHTML = constructionModalTemplate();
 
 // Global components
 GlobalNav.init();
 MiniCart.init();
-Modal.init();
+Modal.init("construction-modal");
