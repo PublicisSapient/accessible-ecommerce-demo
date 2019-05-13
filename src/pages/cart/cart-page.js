d702238f25ce6a84a809bbfde0a59639046001cc
Wesley Cocks <wesley.cocks@publicissapient.com>
2019-05-13 05:33:01 -0600

    feature/134-cart-layout - wjcps - 190513
    Initial blocking in of the cart layout and wiring up of the data

--- src/pages/cart/cart-page.js
+++ src/pages/cart/cart-page.js
@@ -1,8 +1,13 @@
 import { getCartFromStorage } from '../../components/cart/cart';
 import cartItemsTemplate from './cart-items.hbs';
+import cartMoneyBoxTemplate from './cart-moneybox.hbs';
 
 (function cartPage() {
   const cartData = getCartFromStorage();
   const cartItemsEl = document.querySelector('[data-template="cart-items"]');
+  const cartMoneyBoxEl = document.querySelector('[data-template="cart-moneybox"]');
+  //console.log("cartData", cartData);
   cartItemsEl.outerHTML = cartItemsTemplate(cartData);
+  cartMoneyBoxEl.outerHTML = cartMoneyBoxTemplate(cartData);
+
 })();
