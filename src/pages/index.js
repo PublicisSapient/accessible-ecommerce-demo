88323bebfe44c5c5a290d766eb54031e1eb51c69
Marcello Paiva <marcello.paiva@gmail.com>
2020-08-05 11:41:03 -0400

    commented out the keystroke links and replaced them with normal skip links

--- src/pages/index.js
+++ src/pages/index.js
@@ -104,109 +104,109 @@ import * as Modal from '../components/modal/modal';
 
 /* Key Link Functions  */
 
-setTimeout(() => {
-  let keyLinks = document.getElementById('key-link-container');
-  keyLinks.onkeydown = onKeyLinkPress;
-}, 1000)
+// setTimeout(() => {
+//   let keyLinks = document.getElementById('key-link-container');
+//   keyLinks.onkeydown = onKeyLinkPress;
+// }, 1000)
 
-function onKeyLinkPress(e) {
-  console.log('triggered???')
-  if (e.code === 'KeyS') {
-    let node = document.getElementById('global-header__search-input')
-    node && node.focus()
-  }
-  if (e.code === 'KeyP') {
-    let node = document.getElementById('main-nav-women')
-    node && node.focus()
-  }
-  if (e.code === 'KeyR') {
-    let node = document.getElementById('product-link-container')
-    node && node.focus()
-  }
-  if (e.code === 'KeyC') {
-    let node = document.getElementById('mini-cart-button')
-    node && node.focus()
-  }
-  if (e.code === 'KeyA') {
-    let node = document.getElementById('footer_statement-link')
-    node && node.focus()
-  }
-  if (e.code === 'KeyU') {
-    let node = document.getElementById('about-us-link')
-    node && node.focus()
-  }
-  if (e.code === 'ArrowRight' || e.code === 'ArrowDown') {
-    e.preventDefault()
-    nextLink()
-  }
-  if (e.code === 'ArrowLeft' || e.code === 'ArrowUp') {
-    e.preventDefault()
-    prevLink()
-  }
-}
+// function onKeyLinkPress(e) {
+//   console.log('triggered???')
+//   if (e.code === 'KeyS') {
+//     let node = document.getElementById('global-header__search-input')
+//     node && node.focus()
+//   }
+//   if (e.code === 'KeyP') {
+//     let node = document.getElementById('main-nav-women')
+//     node && node.focus()
+//   }
+//   if (e.code === 'KeyR') {
+//     let node = document.getElementById('product-link-container')
+//     node && node.focus()
+//   }
+//   if (e.code === 'KeyC') {
+//     let node = document.getElementById('mini-cart-button')
+//     node && node.focus()
+//   }
+//   if (e.code === 'KeyA') {
+//     let node = document.getElementById('footer_statement-link')
+//     node && node.focus()
+//   }
+//   if (e.code === 'KeyU') {
+//     let node = document.getElementById('about-us-link')
+//     node && node.focus()
+//   }
+//   if (e.code === 'ArrowRight' || e.code === 'ArrowDown') {
+//     e.preventDefault()
+//     nextLink()
+//   }
+//   if (e.code === 'ArrowLeft' || e.code === 'ArrowUp') {
+//     e.preventDefault()
+//     prevLink()
+//   }
+// }
 
-function nextLink() {
-  let next
-  switch (document.activeElement.id) {
-    case 's4search':
-      next = document.getElementById('r4results')
-      next.focus()
-      break;
-    case 'r4results':
-      next = document.getElementById('p4products')
-      next.focus()
-      break;
-    case 'p4products':
-      next = document.getElementById('c4cart')
-      next.focus()
-      break;
-    case 'c4cart':
-      next = document.getElementById('a4a11y')
-      next.focus()
-      break;
-    case 'a4a11y':
-      next = document.getElementById('u4about')
-      next.focus()
-      break;
-    case 'u4about':
-      break;
-    default:
-      next = document.getElementById('s4search')
-      next.focus()
-      break;
-  }
-}
+// function nextLink() {
+//   let next
+//   switch (document.activeElement.id) {
+//     case 's4search':
+//       next = document.getElementById('r4results')
+//       next.focus()
+//       break;
+//     case 'r4results':
+//       next = document.getElementById('p4products')
+//       next.focus()
+//       break;
+//     case 'p4products':
+//       next = document.getElementById('c4cart')
+//       next.focus()
+//       break;
+//     case 'c4cart':
+//       next = document.getElementById('a4a11y')
+//       next.focus()
+//       break;
+//     case 'a4a11y':
+//       next = document.getElementById('u4about')
+//       next.focus()
+//       break;
+//     case 'u4about':
+//       break;
+//     default:
+//       next = document.getElementById('s4search')
+//       next.focus()
+//       break;
+//   }
+// }
 
-function prevLink() {
-  let next
-  switch (document.activeElement.id) {
-    case 's4search':
-      break;
-    case 'r4results':
-      next = document.getElementById('s4search')
-      next.focus()
-      break;
-    case 'p4products':
-      next = document.getElementById('r4results')
-      next.focus()
-      break;
-    case 'c4cart':
-      next = document.getElementById('p4products')
-      next.focus()
-      break;
-    case 'a4a11y':
-      next = document.getElementById('c4cart')
-      next.focus()
-      break;
-    case 'u4about':
-      next = document.getElementById('a4a11y')
-      next.focus()
-      break;
-    default:
-      next = document.getElementById('u4about')
-      next.focus()
-      break;
-  }
-}
+// function prevLink() {
+//   let next
+//   switch (document.activeElement.id) {
+//     case 's4search':
+//       break;
+//     case 'r4results':
+//       next = document.getElementById('s4search')
+//       next.focus()
+//       break;
+//     case 'p4products':
+//       next = document.getElementById('r4results')
+//       next.focus()
+//       break;
+//     case 'c4cart':
+//       next = document.getElementById('p4products')
+//       next.focus()
+//       break;
+//     case 'a4a11y':
+//       next = document.getElementById('c4cart')
+//       next.focus()
+//       break;
+//     case 'u4about':
+//       next = document.getElementById('a4a11y')
+//       next.focus()
+//       break;
+//     default:
+//       next = document.getElementById('u4about')
+//       next.focus()
+//       break;
+//   }
+// }
 
 /* End Key Link Functions */
